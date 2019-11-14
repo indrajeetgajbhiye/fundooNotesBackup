9e3f897f65b9913c0686b7bca50584fa2e9e51ff
indrajeetgajbhiye <indrajeetgajbhiye@gmail.com>
2019-11-14 12:11:45 +0530

    done

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express()
 app.use(express.static(__dirname + "/dist"))
 
 app.listen(process.env.PORT || 8080)
+
+// path location strategy 
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + 'dist/index.html'))
+})
+
+console.log('listnening on port')
