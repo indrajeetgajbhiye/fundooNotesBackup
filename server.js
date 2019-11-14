00c3611eab5368b5f3607e6f1c724b8746d8c6ce
indrajeetgajbhiye <indrajeetgajbhiye@gmail.com>
2019-11-14 12:52:09 +0530

    added pwa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require('path')
-app.use(express.static(__dirname + "/dist"))
+app.use(express.static(path.join(__dirname + "/dist/fundooNotes")))
 
 app.listen(process.env.PORT || 8080)
 
